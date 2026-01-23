6011eca426122a3de529f1289e3930d1528d016a
Dhruv Shah <dhruvkshah75@gmail.com>
2026-01-23 16:34:27 +0530

    Started working on the cli-version of TaskFlow

---
+++ taskflow-cli/cli.py
@@ -0,0 +1,7 @@
+import typer
+from rich.console import Console
+
+# initialise the app and console
+app = typer.Typer(help="TaskFlow CLI - Manage your tasks from the terminal") 
+console = Console()
+
